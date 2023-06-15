5e8b0aa96db283a237992f4a6f28b110a691adf7
Dzejkop02 <104920891+Dzejkop02@users.noreply.github.com>
2023-06-15 19:12:20 +0200

    Lab 11

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Counter from "./components/counter";
-import Timer from "./components/Timer"
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import { isExpired } from "react-jwt";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import LoginForm from "./components/loginForm";
+import NotFound from "./components/notFound";
+import SignUpForm from "./components/signUp";
 
-ReactDOM.render(
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Counter />
-    <Timer />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App/>}>
+                  <Route path="home" element={<Home/>}/>
+                  <Route path="posts" element={isExpired(localStorage.getItem('token')) ? <Navigate replace to="/home"/> : <Posts/>}/>
+                  <Route path="login" element={<LoginForm/>}/>
+                  <Route path="signUp" element={<SignUpForm/>}/>
+                  <Route
+                      path="*"
+                      element={
+                          <NotFound/>
+                      }
+                  />
+              </Route>
+          </Routes>
+      </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
